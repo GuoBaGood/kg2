0870b47569e6d979ad6e1e64f3fe78b9045291b6
hxlyyl <hxl963369@163.com>
2019-01-02 22:17:06 +0800

    ok

---
+++ src/main/java/com/hxl/IndexCotroller.java
@@ -0,0 +1,30 @@
+package com.hxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author 锅巴不会糊
+ *
+ */
+
+//自动配置，无须xml文件
+@EnableAutoConfiguration
+@Controller
+public class IndexCotroller {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String ss() {
+		return "I am a good Student!";
+	}
+	
+	
+	//入口
+	public static void main(String args[]) {
+		SpringApplication.run(IndexCotroller.class, args);
+	}
+}
